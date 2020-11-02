651c63196a64a9e01457a4a78d767bed5e31549f
Ryan How <ryanhow1999@yahoo.ca>
2020-11-01 23:23:48 -0500

    on mount, retrieve posts from database. error handling and small ui changes added

--- screens/HomeFeedScreen.tsx
+++ screens/HomeFeedScreen.tsx
@@ -1,52 +1,50 @@
 import * as React from "react";
-import { useState } from "react";
-import { StyleSheet, TextInput, Text, View, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 import { Posting } from "./../components/Posting";
+import axios from "axios";
 
-const postings = [
-  {
-    tags: ["bread", "toast"],
-    title: "Bread",
-    requesting: true,
-    location: "Icon - Waterloo",
-    date: new Date(),
-    content: "Ran out of bread. Looking for two slices!",
-    created_by: {
-      _id: "5f9502bde30f82462ae7056c",
-      user_id: 2,
-      username: "jesssmith123",
-      full_name: "Jessica Smith",
-      avatar: "female3.png"
-    },
-    status: "active",
-    comments: [],
-    post_id: "1"
-  },
-  {
-    tags: [],
-    title: "Apples",
-    post_id: "6",
-    requesting: true,
-    content: "Need apples ",
-    location: "E7",
-    created_by: {
-      _id: "5f962ade1e5c522e748f40d3"
-    },
-    date: new Date(),
-    status: "active",
-    comments: []
-  },
-  {
-    title: "Printer",
-    post_id: "10",
-    requesting: false,
-    content: "Willing to let people print stuff!",
-    location: "Blair House"
-  }
-];
+interface Post {
+  post_id: string;
+  title: string;
+  requesting: boolean;
+  content: string;
+  location: string;
+  created_by: object;
+  date: Date;
+  status: string;
+  tags: string[];
+  comments: object[];
+}
 
 export default function HomeFeedScreen() {
   const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    console.log("here");
+    axios
+      .get("http://192.168.2.91:3000/api/v1/posts")
+      .then(resp => {
+        //console.log(resp.data);
+        setPosts(resp.data);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -57,16 +55,7 @@ export default function HomeFeedScreen() {
           <Text style={styles.topSecondaryText}>Filter</Text>
         </View>
         <TextInput
-          style={{
-            height: 60,
-            borderColor: "black",
-            borderWidth: 2,
-            marginHorizontal: 20,
-            marginTop: 30,
-            borderRadius: 50,
-            padding: 10,
-            backgroundColor: "white"
-          }}
+          style={styles.search}
           placeholder="Search"
           placeholderTextColor="black"
           onChangeText={searchText => setSearch(searchText)}
@@ -76,24 +65,43 @@ export default function HomeFeedScreen() {
       <View style={styles.feed}>
         <View style={styles.newPostingsContainer}>
           <Text style={styles.postingSubtitle}>New Postings</Text>
-          <View style={styles.postings}>
-            <ScrollView horizontal={true}>
-              {postings.map(post => (
-                <Posting key={post.post_id} post={post}></Posting>
-              ))}
-            </ScrollView>
-          </View>
+          {error ? (
+            <Text>Error retrieving posts</Text>
+          ) : posts.length === 0 ? (
+            <Text>No results available</Text>
+          ) : (
+            <View style={styles.postings}>
+              <ScrollView horizontal={true}>
+                {posts.map(post => (
+                  <Posting key={post.post_id} post={post}></Posting>
+                ))}
+              </ScrollView>
+            </View>
+          )}
         </View>
         <View style={styles.trendingContainer}>
           <Text style={styles.postingSubtitle}>Trending</Text>
-          <View style={styles.postings}>
-            <ScrollView horizontal={true}>
-              {postings.map(post => (
-                <Posting key={post.post_id} post={post}></Posting>
-              ))}
-            </ScrollView>
-          </View>
+          {error ? (
+            <Text>Error retrieving posts</Text>
+          ) : posts.length === 0 ? (
+            <Text>No results available</Text>
+          ) : (
+            <View style={styles.postings}>
+              <ScrollView horizontal={true}>
+                {posts.map(post => (
+                  <Posting key={post.post_id} post={post}></Posting>
+                ))}
+              </ScrollView>
+            </View>
+          )}
         </View>
+      </View>
+      <View style={styles.nav}>
+        <TouchableOpacity>
+          <View style={styles.circle}>
+            <Text style={styles.plusSign}>+</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -123,6 +131,16 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 17.5
   },
+  search: {
+    height: 60,
+    borderColor: "black",
+    borderWidth: 2,
+    marginHorizontal: 20,
+    marginTop: 30,
+    borderRadius: 50,
+    padding: 10,
+    backgroundColor: "white"
+  },
   feed: {
     flex: 6
   },
@@ -139,5 +157,20 @@ const styles = StyleSheet.create({
   },
   postings: {
     flexDirection: "row"
+  },
+  nav: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  circle: {
+    backgroundColor: "#EB5757",
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: "center"
+  },
+  plusSign: {
+    fontSize: 75,
+    color: "white"
   }
 });
