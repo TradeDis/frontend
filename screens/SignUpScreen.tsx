3060c6da1d0a63351dd2e5384fb64229de31b5d4
Gary Yong <z2yong@uwaterloo.ca>
2020-11-07 13:53:25 -0800

    fix: local env file not read
    Because of the missing semicolon, both the sign-up and sign-in page could not read the local env file properly, leading to 404 errors, and unhandled Rejection (TypeError)

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import axios from "axios";
-import { API_URL } from "@env"
+import { API_URL } from "@env";
 
 import {
     StyleSheet,
