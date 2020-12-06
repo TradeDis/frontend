1baea44a593adae58d454130adf2b3eb44520cdf
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-12-05 20:30:54 -0500

    Add inbox filter by name/post

--- screens/InboxScreen.tsx
+++ screens/InboxScreen.tsx
@@ -1,49 +1,76 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import {
-  StyleSheet, View, Text, TouchableOpacity, ScrollView, RefreshControl,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  RefreshControl,
 } from "react-native";
 import { Card, ListItem, Avatar } from "react-native-elements";
-import Loading from '../components/Loading';
-import useStatsBar from '../hooks/useStatusBar';
+import Loading from "../components/Loading";
+import useStatsBar from "../hooks/useStatusBar";
 import axios from "axios";
-import { API_URL } from "@env"
-import BottomNavigation from '../components/BottomNavigation';
-import { AuthContext } from '../navigation/AuthProvider';
-import { TextInput, Button } from 'react-native-paper';
+import { API_URL } from "@env";
+import BottomNavigation from "../components/BottomNavigation";
+import { AuthContext } from "../navigation/AuthProvider";
+import { TextInput, Button, Searchbar } from "react-native-paper";
 
 export default function InboxScreen({ navigation }) {
-  useStatsBar('light-content');
+  useStatsBar("light-content");
 
   const [conversations, setConversations] = useState([]);
   const [loading, setLoading] = useState(true);
   const { user, setUser } = useContext(AuthContext);
   const [refreshing, setRefreshing] = React.useState(false);
 
+  const [filteredConversations, setFilteredConversations] = useState(
+    conversations
+  );
+
+  const filterConversations = (searchText) => {
+    const search = searchText.toLowerCase();
+    const filtered = conversations.filter((convo) => {
+      let memberFilter = false;
+      convo.members.forEach((m) => {
+        if (m.name.toLowerCase().includes(search)) {
+          memberFilter = true;
+        }
+      });
+      return convo.name.toLowerCase().includes(search) || memberFilter;
+    });
+    setFilteredConversations(filtered);
+  };
+
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
-    loadConversations()
+    loadConversations();
   }, []);
 
   const loadConversations = async () => {
     axios
-      .get(`http://192.168.31.138:3000/api/v1/users/${user.user_id}/conversations`)
-      .then(resp => {
+      .get(
+        `http://192.168.31.138:3000/api/v1/users/${user.user_id}/conversations`
+      )
+      .then((resp) => {
         // console.log(resp.data)
-        setConversations(resp.data)
+        setConversations(resp.data);
+        setFilteredConversations(resp.data);
       })
-      .catch(err => {
-        const { errors } = err.response.data
-      }).finally(() => {
-        setLoading(false)
+      .catch((err) => {
+        const { errors } = err.response.data;
+      })
+      .finally(() => {
+        setLoading(false);
         setRefreshing(false);
-      })
-  }
+      });
+  };
 
   /**
    * Fetch threads from Firestore
    */
   useEffect(() => {
-    loadConversations()
+    loadConversations();
   }, []);
 
   if (loading) {
@@ -54,24 +81,40 @@ export default function InboxScreen({ navigation }) {
     <View style={styles.container}>
       <View style={styles.main}>
         <View style={styles.head}>
-          <Button icon="filter" mode="contained" style={styles.filter}>Filter</Button>
+          <Searchbar
+            style={styles.search}
+            placeholder="Search"
+            placeholderTextColor="black"
+            onChangeText={(searchText) => filterConversations(searchText)}
+          />
         </View>
-        <ScrollView style={styles.scrollView}
+        <ScrollView
+          style={styles.scrollView}
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         >
-          {conversations.map((c, i) => {
-            let non_me = c.members.filter(m => m.user_id != user.user_id)
+          {filteredConversations.map((c, i) => {
+            let non_me = c.members.filter((m) => m.user_id != user.user_id);
             if (non_me.length < 1) {
-              non_me = [{
-                ...user,
-                name: user.first_name + " " + user.last_name
-              }]
+              non_me = [
+                {
+                  ...user,
+                  name: user.first_name + " " + user.last_name,
+                },
+              ];
             }
             return (
-              <ListItem key={i} bottomDivider onPress={() => navigation.navigate('Room', { conversation: c })}>
-                <Avatar source={{ uri: `https://ui-avatars.com/api/?background=random&rounded=true&name=${non_me[0].name}` }} />
+              <ListItem
+                key={i}
+                bottomDivider
+                onPress={() => navigation.navigate("Room", { conversation: c })}
+              >
+                <Avatar
+                  source={{
+                    uri: `https://ui-avatars.com/api/?background=random&rounded=true&name=${non_me[0].name}`,
+                  }}
+                />
                 <ListItem.Content>
                   <ListItem.Title style={{ fontWeight: "bold" }}>
                     {c.name}
@@ -81,14 +124,24 @@ export default function InboxScreen({ navigation }) {
                     {c.members.map(member => member.name).join(' & ')}
                   </Text>
                 </ListItem.Subtitle> */}
-                  <ListItem.Subtitle numberOfLines={1} style={{ color: "gray" }}>
+                  <ListItem.Subtitle
+                    numberOfLines={1}
+                    style={{ color: "gray" }}
+                  >
                     <Text>
-                      {c.latestMessage ? (c.latestMessage.user.name == user.first_name + " " + user.last_name ? "You" : c.latestMessage.user.name) + ": " + c.latestMessage.text : ''}
+                      {c.latestMessage
+                        ? (c.latestMessage.user.name ==
+                          user.first_name + " " + user.last_name
+                            ? "You"
+                            : c.latestMessage.user.name) +
+                          ": " +
+                          c.latestMessage.text
+                        : ""}
                     </Text>
                   </ListItem.Subtitle>
                 </ListItem.Content>
               </ListItem>
-            )
+            );
           })}
         </ScrollView>
       </View>
@@ -104,7 +157,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 80,
     marginBottom: 30,
-    marginVertical: 40
+    marginVertical: 40,
   },
   container: {
     flex: 1,
@@ -136,11 +189,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
   },
-
+  search: {
+    width: "100%",
+    borderRadius: 10,
+  },
   filter: {
     // color: "#fff",
     // backgroundColor: "#EB5757",
     borderRadius: 5,
     margin: 10,
-  }
+  },
 });
